4fe7b072984f175954313e6991ec2c260f95382e
davidfp123 <david.fzpz@gmail.com>
2018-10-09 14:39:06 +0200

    First commit

---
+++ davidfp123/src/test/java/es/upm/grise/profundizacion2018/AppTest.java
@@ -0,0 +1,20 @@
+package es.upm.grise.profundizacion2018;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
